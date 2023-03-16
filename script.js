1e0eb566d875d6109179f0fb708bd56969c294af
Bertinkambale <bertinkambale988@gmail.comgit config --global user.email bertinkambale988@gmail.com>
2023-03-16 17:55:12 +0200

    add text to the teacherAbout

--- script.js
+++ script.js
@@ -15,37 +15,37 @@ const Teachers = [{
   teacherImagee: 'img-folder/teacher1.jpg',
   teachearName: 'Bella mwa',
   teacherDomain: 'her,english teacher',
-  teacherAbout: 'bella is best teacher of english she is from USA , she studied in havard university,she has a lot of experiences in teaching Englihs',
+  teacherAbout: 'bella is best teacher of english she is from USA.',
 },
 {
   teacherImagee: 'img-folder/teacher2.jpg',
   teachearName: 'Bertin kam',
   teacherDomain: 'him,spanish teacher',
-  teacherAbout: 'bertin is best teacher of spanish he is from Spain , he studied in Ukinsad university,he has a lot of experiences in teaching Spanish',
+  teacherAbout: 'bertin is best teacher of spanish he is from Spain.',
 },
 {
   teacherImagee: 'img-folder/teacher3.jfif',
   teachearName: 'Linda ker',
   teacherDomain: 'her,Arbic teacher',
-  teacherAbout: 'Linda is best teacher of Arbic she is from Arabi , she studied in USIMA university,she has a lot of experiences in teaching Arbic',
+  teacherAbout: 'Linda is best teacher of Arbic she is from Arabi.',
 },
 {
   teacherImagee: 'img-folder/teacher4.jfif',
   teachearName: 'Benjamin nyam',
   teacherDomain: 'him,Lingala teacher',
-  teacherAbout: 'benjamin is best teacher of lingala he is from DRC , he studied in Unikin university,he has a lot of experiences in teaching Lingala',
+  teacherAbout: 'benjamin is best teacher of lingala he is from DRC.',
 },
 {
   teacherImagee: 'img-folder/teacher5.jfif',
   teachearName: 'kevin Zam',
   teacherDomain: 'him,french teacher',
-  teacherAbout: 'kevin is best teacher of french he is from France , he studied in LOURD university,he has a lot of experiences in teaching French',
+  teacherAbout: 'kevin is best teacher of french he is from France.',
 },
 {
   teacherImagee: 'img-folder/teacher6.jfif',
   teachearName: 'Rachel Bim',
   teacherDomain: 'her,Italiano teacher',
-  teacherAbout: 'Rachel is best teacher of Italiano she is from Italia , she studied in Roma university,she has a lot of experiences in teaching Italiano',
+  teacherAbout: 'Rachel is best teacher of Italiano she is from Italia.',
 },
 ];
 
